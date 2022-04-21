34e2557391d00179b290d23f499199e2346e7832
KitsuneKeira21 <104032980+KitsuneKeira21@users.noreply.github.com>
2022-04-20 17:41:25 -0700

    Add files via upload

---
+++ python/table1.py
@@ -0,0 +1,37 @@
+# *KitsuneKeira*
+#
+# This program links to table1 in index.html & commands it based on user input
+#
+# Declare integers
+
+
+gridContainer = 'grid-container'
+container = ''
+table = '' 
+tableData = 'td'
+tableHeader = 'th'
+row = ''
+pagination = ''
+pageItem = 'page-item'
+tableHead = 'thead'
+tableBody = 'tbody'
+navigate = 'nav'
+page1 = ''
+page2 = ''
+page3 = ''
+
+id="table1.table1.py" 
+
+def open(file:"index.html",) 
+  
+
+def vars (table: container) -> row(pagination):
+
+    
+
+
+
+# filter(function, iterable)
+# format()
+# hasattr()
+# issubclass()
